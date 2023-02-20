3a109f86f21968e3764fe7cef30e597b345f8114
kindletheworld <samia.ummariam@gmail.com>
2023-02-20 17:29:22 +0300

    moving to github

---
+++ printFunc.py
@@ -0,0 +1,10 @@
+#Using print funciton to display a text message on screen
+print("Hello World!")
+
+#Define a variable and printing its value
+myIntVar=365
+print(myIntVar)
+
+#Declaring a string variable
+myString="Welcome to KindleTheWorld"
+print(myString)
